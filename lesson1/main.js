d24842a071c1e78c39982f706df94ccfaaff33d6
AlexeyKorotkov <alexlera@060511@gmail.com>
2023-05-02 15:36:18 +0300

    add lesson 1 to project

---
+++ lesson1/main.js
@@ -0,0 +1,50 @@
+let a='hello'
+console.log(a)
+let b='own'
+console.log(b)
+let c='com'
+console.log(c)
+let d='ua'
+console.log(d)
+let a1=1
+console.log(a1)
+let b1=10
+console.log(b1)
+let c1=-999
+console.log(c1)
+let d1=123
+console.log(d1)
+let e1=3.14
+console.log(e1)
+let f1=2.7
+console.log(f1)
+let g1=16
+console.log(g1)
+let e=true
+console.log(e)
+let f=false
+console.log(f)
+
+
+let firstName='Alexey'
+let middleName='Alexandrovich'
+let lastName='Korotkov'
+let user= firstName +' '+ middleName +' '+ lastName
+console.log(user)
+
+
+let a2=100
+console.log(typeof a2)
+let b2='100'
+console.log(typeof b2)
+let c2=true
+console.log(typeof c2)
+
+
+
+
+
+
+
+
+
